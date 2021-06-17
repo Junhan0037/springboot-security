d8d8b0f4a88f0d4f03b9466e7df6cf5ea8e52864
Junhan0037 <wnsgks0037@naver.com>
2021-06-17 21:44:07 +0900

    feat: 권한설정과 표현식
    -

--- basicsecurity/src/main/java/com/basicsecurity/SecurityConfig.java
+++ basicsecurity/src/main/java/com/basicsecurity/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.basicsecurity;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,8 +19,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("{noop}1111").roles("USER");
+        auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS");
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/admin/pay").hasRole("ADMIN") // 구체적인 경로를 먼저 선언
+                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
                 .anyRequest().authenticated();
 
         http.formLogin()
--- basicsecurity/src/main/java/com/basicsecurity/SecurityController.java
+++ basicsecurity/src/main/java/com/basicsecurity/SecurityController.java
@@ -16,4 +16,19 @@ public class SecurityController {
         return "loginPage";
     }
 
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin/pay")
+    public String adminPay() {
+        return "adminPay";
+    }
+
+    @GetMapping("/admin/**")
+    public String admin() {
+        return "admin";
+    }
+
 }
