9bb3f5ba2a4a725c066efd5fa085e0396792547d
Junhan0037 <wnsgks0037@naver.com>
2021-07-08 08:54:23 +0900

    feat: init Project
    - 실전 프로젝트 생성

---
+++ spring-core-security/src/test/java/com/springcoresecurity/SpringCoreSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcoresecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
